ee4e0dd848c86b2fb6d38e6fe3bf2de22710dfab
Mateusz Matejko <matejko.mateusz7456@gmail.com>
2023-10-04 18:01:12 +0200

    new data added

---
+++ project/web_scrappers/main.py
@@ -0,0 +1,8 @@
+from gratka_gdansk import main as gratka_gdansk
+from gratka_krakow import main as gratka_krakow
+from trojmiasto import main as trojmaisto
+from olx_gdansk_collector import main as olx_gdansk
+from olx_krakow_collector import main as olx_krakow
+from otodom_gdansk_collector import main as otodom_gdansk
+from otodom_krakow_collector import main as otodom_krakow
+
